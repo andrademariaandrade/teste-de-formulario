d0d48574142c7794ed79217b690691d6f3bb1608
andrademariaandrade <165902324+andrademariaandrade@users.noreply.github.com>
2024-10-23 08:47:53 -0300

    teste1

---
+++ script.js
@@ -0,0 +1,6 @@
+// validação do fomulário
+function validateform(){
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let pasword = document.getElementById("password").value;
+}
